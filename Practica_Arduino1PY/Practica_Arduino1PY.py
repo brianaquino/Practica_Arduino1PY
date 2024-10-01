ccce1e81d573136fa87580e4202d75a48f2f9c2f
brianaquino <brian.aquino50@unach.mx>
2024-10-01 11:51:52 -0600

    Agregar archivos de proyecto.

---
+++ Practica_Arduino1PY/Practica_Arduino1PY.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import serial
+# configuracion del puerto en serie
+
+port = 'COM4'
+baudrate = 9600
+ser = serial.Serial(port, baudrate)
+
+def encender_led():
+    ser.write(b'1')
+    print("LED Encendido")
+
+def apagar_led():
+    ser.write(b'0')
+    print("LED Apagado")
+
+ventana = tk.Tk()
+ventana.title("Control de LED")
+
+boton_encender = tk.Button(ventana, text = "Encender LED", command=encender_led)
+boton_encender.pack(pady=10)
+
+boton_apagar = tk.Button(ventana, text = "Apagar LED", command=apagar_led)
+boton_apagar.pack(pady=10)
+
+ventana.mainloop()
+
+ser.close()
